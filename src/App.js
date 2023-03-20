15729a60eae94c18ec2a6bbe57ae4d8181648b5d
Abdullah <p180013@nu.edu.pk>
2023-03-20 07:12:38 +0500

    msg

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './App.css';
+import { selectUser } from './features/userSlice';
 import Feed from './Feed';
 import Header from './Header';
+import Login from './Login';
 import Sidebar from './Sidebar';
 
 function App() {
+
+    const user= useSelector(selectUser);
+
   return ( 
     <div className="app">
     {/* Header */}
     <Header />
-    {/* App Body*/}
+    {!user ? (
+
+    <Login />
+    ):(
+         // {/* App Body*/}
     <div className='app__body'>
       {/* Sidebar*/ }
       <Sidebar />
@@ -17,6 +27,8 @@ function App() {
       <Feed />
     {/* Widgets*/ }
     </div>
+    )}
+
     
     </div>
   );
